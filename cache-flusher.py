ca303b9a7acf2111072d51ebf93dadf1195f822a
Jordan Shaw <jrdnshw@gmail.com>
2012-11-26 17:58:43 -0500

    Updated and cleaned up the code. Put the calls into a class and added more descriptive comments.

--- cache-flusher.py
+++ cache-flusher.py
@@ -1,102 +1,108 @@
-# Flush the Facebook Cache for every pate in the
-# using http://www.xml-sitemaps.com/ to generate page site maps.
+# Flush the Facebook Cache for the pages supplied in the site map(s) located in the sitemaps directory.
+# Using http://www.xml-sitemaps.com/ to generate page site maps.
 
 import os
 import glob
-# If you want to set a timeout for the requests import time
-# import time
+import time
 import urllib
 import requests
 import xml.etree.ElementTree as ET
 
-# Social URLs + UTMs
 
+class FlushFacebookCache:
 
-def tag_uri_and_name(elem):
-    if elem.tag[0] == "{":
-        uri, ignore, tag = elem.tag[1:].partition("}")
-    else:
-        uri = None
-        tag = elem.tag
-    return uri, tag
+    def __init__(self):
+        # Facebooks Debugger URL
+        self.fb_url = "https://developers.facebook.com/tools/debug/og/object"
 
+        # Sets user agent
+        self.headers = {}
+        self.headers['User-Agent'] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:16.0) Gecko/20100101 Firefox/16.0"
 
-# Facebooks Debugger URL
-fb_url = "https://developers.facebook.com/tools/debug/og/object"
+        # Declairs the required array for storing files & sitemap page URLs
+        self.files = []
+        self.all_pages = []
 
-# Sets user agent
-headers = {}
-headers['User-Agent'] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:16.0) Gecko/20100101 Firefox/16.0"
+        # Delay before next request?
+        self.use_timer = False
 
-# ET._namespace_map["http://www.w3.org/2001/XMLSchema-instance"] = 'xsi'
+    def tag_uri_and_name(self, elem):
+        if elem.tag[0] == "{":
+            uri, ignore, tag = elem.tag[1:].partition("}")
+        else:
+            uri = None
+            tag = elem.tag
+        return uri, tag
 
-# Declairs the required array for storing files & sitemap page URLs
-files = []
-all_pages = []
+    def get_files(self):
+        # Loops through all XML files in specified directory
+        # Get all sitemap files and names
+        os.chdir("sitemaps")
+        for file in glob.glob("*.xml"):
+            self.files.append(file)
 
-# Loops through all XML files in specified directory
-# Loads the XML
+        self.get_site_urls()
 
-# Get all sitemap files and names
-os.chdir("sitemaps")
-for file in glob.glob("*.xml"):
-    files.append(file)
+    def make_request(self):
+        count = 0
+        while (count < len(self.all_pages)):
+            # If you want to add a timeout while requesting the debugger
+            if(self.use_timer):
+                time.sleep(1.0)
 
+            # Full URL request including FB debugger and params
+            full_url = self.fb_url + "?q=" + self.all_pages[count]
+            print full_url
+            r = requests.get(full_url, headers=self.headers)
 
-# Loops through all of the sitemap files in the directory
-for file in files:
-    path = 'sitemaps/' + file
-    tree = ET.parse(file)
-    root = tree.getroot()
+            # Use if payload is in an object & param order doesn't matter
+            # Comment out above request and uncomment the line below
+            # r = requests.get(full_url, params=self.all_pages[count])
 
-    # gets the XML namespace from the file
-    uri, tag = tag_uri_and_name(root)
+            print r.status_code
+            print r.url
+            print "-----------------"
+            count += 1
 
-    if (uri):
-        uri = '{' + uri + '}'
-        url_namespace = uri + 'url'
-        loc_namespace = uri + 'loc'
-    else:
-        url_namespace = 'url'
-        loc_namespace = 'loc'
+    def get_site_urls(self):
 
-    # Gets all of the URLs in each sitemap xml file
-    # for url in root.findall('url'):
-    for url in root.findall(url_namespace):
-        for loc in url.findall(loc_namespace):
-            url = loc.text
+        # Loops through all of the sitemap files in the directory
+        for file in self.files:
+            tree = ET.parse(file)
+            root = tree.getroot()
 
-            # This sorts the fields alphabetically, so not good for generating custom URLS for submitting to Facebook
-            # paramaters = {
-            #     # 'q': escaped_url,
-            #     'utm_source': 'source_name',
-            #     'utm_medium': 'medium_name',
-            #     'utm_campaign': 'campaign_name'
-            # }
+            # Gets the XML namespace from the file
+            uri, tag = self.tag_uri_and_name(root)
 
-            paramaters = urllib.urlencode([('utm_source', 'source_name'), ('utm_medium', 'medium_name'), ('utm_campaign', 'campaign_name')])
+            if (uri):
+                uri = '{' + uri + '}'
+                url_namespace = uri + 'url'
+                loc_namespace = uri + 'loc'
+            else:
+                url_namespace = 'url'
+                loc_namespace = 'loc'
 
-            # use for an object payload not array
-            # payload = url + "?" + urllib.quote(urllib.urlencode(paramaters), '')
-            payload = url + "?" + urllib.quote(paramaters, '')
+            # Gets all of the URLs in each sitemap xml file
+            for url in root.findall(url_namespace):
+                for loc in url.findall(loc_namespace):
+                    url = loc.text
 
-            all_pages.append(payload)
+                    paramaters = urllib.urlencode([('utm_source', 'source_name'), ('utm_medium', 'medium_name'), ('utm_campaign', 'campaign_name')])
+                    payload = url + "?" + urllib.quote(paramaters, '')
 
+                    # This sorts the fields alphabetically, so not good for generating custom URLS for submitting to Facebook
+                    # Uncomment parameters and other payload below and comment the previous 2 lines if you want to use the object params
+                    # paramaters = {
+                    #     'utm_source': 'source_name',
+                    #     'utm_medium': 'medium_name',
+                    #     'utm_campaign': 'campaign_name'
+                    # }
+                    # Use for an object payload not array
+                    # payload = url + "?" + urllib.quote(urllib.urlencode(paramaters), '')
 
-count = 0
-while (count < len(all_pages)):
-    #If you want to add a timeout while requesting the debugger
-    # time.sleep(0.1)
+                    self.all_pages.append(payload)
 
-    # Full URL request including FB debugger and params
+        self.make_request()
 
-    full_url = fb_url + "?q=" + all_pages[count]
-    r = requests.get(full_url, headers=headers)
-
-    # use if payload is in an object & param order doesn't matter
-    # r = requests.get(full_url, params=payload)
-
-    print r.status_code
-    print r.url
-    print "-----------------"
-    count += 1
+flush_cache = FlushFacebookCache()
+flush_cache.get_files()
